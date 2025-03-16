330a0db5d5caee3d398a6000ec0a062b481406b6
CosminaV <114497479+CosminaV@users.noreply.github.com>
2025-03-17 01:25:11 +0200

    chore(frontend): refactor

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,35 +1,31 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import api from "../api";
 
 const Dashboard = () => {
     const [error, setError] = useState("");
+    const [data, setData] = useState("");
 
-    const fetchProtectedData = async (e) => {
-        e.preventDefault();
-        setError("");
+    const fetchProtectedData = async () => {
         try {
             const response = await api.get("/demo");
-            setError(response.data);
+            setData(response.data);
         } catch (err) {
             console.error("Error fetching protected data:", err);
-            if (err.response) {
-                const data = err.response.data;
-
-                if (data.error) {
-                    setError(data.error);
-                } else {
-                    setError("Unauthorized.");
-                }
-            } else {
-                setError("Server error. Please try again later.");
+            if (err.response.data.error) {
+                setError("Unauthorized");
             }
         }
     };
 
+    useEffect(() => {
+        fetchProtectedData();
+    }, []);
+
     return (
         <div>
             <h2>Dashboard</h2>
-            <button onClick={fetchProtectedData}>Get Protected Data</button>
+            <button onClick={fetchProtectedData}>Get protected data</button>
+            {data && <p>{data}</p>}
             {error && <p className="error">{error}</p>}
         </div>
     );
