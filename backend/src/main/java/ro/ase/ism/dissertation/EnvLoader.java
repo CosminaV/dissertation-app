704fb1d82d61ac28f29552cfe61765d7cb5cd3c5
CosminaV <114497479+CosminaV@users.noreply.github.com>
2025-03-30 04:18:57 +0300

    feat(backend): vault - configured tls

--- backend/src/main/java/ro/ase/ism/dissertation/EnvLoader.java
+++ backend/src/main/java/ro/ase/ism/dissertation/EnvLoader.java
@@ -4,14 +4,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvLoader {
     public static void loadEnv() {
-        Dotenv dotenv = Dotenv.configure()
+        Dotenv.configure()
                 .directory("src/main/resources")
                 .filename(".env")
                 .ignoreIfMissing()
                 .systemProperties()
                 .load();
-
-        String vaultToken = dotenv.get("VAULT_TOKEN");
-        System.setProperty("VAULT_TOKEN", vaultToken);
     }
 }
