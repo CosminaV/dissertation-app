c54c33e7e6159c036a2a20f64689843a11d590dc
CosminaV <114497479+CosminaV@users.noreply.github.com>
2025-04-26 18:41:37 +0300

    fix(backend): use hex ti byte array instead of Base64 decoding in JwtService

--- backend/src/main/java/ro/ase/ism/dissertation/service/JwtService.java
+++ backend/src/main/java/ro/ase/ism/dissertation/service/JwtService.java
@@ -132,7 +132,17 @@ public class JwtService {
     }
 
     private SecretKey getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        byte[] keyBytes = hexStringToByteArray(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
     }
 }
