ff6217851e0c80b6b4a8c9e07e9366db14fceb13
CosminaV <114497479+CosminaV@users.noreply.github.com>
2025-05-01 18:49:25 +0300

    feat(backend): added teacher service that performs access validations

---
+++ backend/src/main/java/ro/ase/ism/dissertation/service/teacher/TeacherAccessService.java
@@ -0,0 +1,35 @@
+package ro.ase.ism.dissertation.service.teacher;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+import ro.ase.ism.dissertation.model.course.CourseGroup;
+import ro.ase.ism.dissertation.model.coursecohort.CourseCohort;
+import ro.ase.ism.dissertation.model.material.Material;
+
+@Service
+@RequiredArgsConstructor
+public class TeacherAccessService {
+
+    void validateTeacherAccessToCourseGroup(CourseGroup courseGroup, Integer teacherId) {
+        if (!courseGroup.getPracticalTeacher().getId().equals(teacherId)) {
+            String groupName = courseGroup.getStudentGroup().getName();
+            String courseName = courseGroup.getCourse().getName();
+            throw new AccessDeniedException("You are not allowed to access practical materials for course '" + courseName + "' and group '" + groupName + "'");
+        }
+    }
+
+    void validateTeacherAccessToCourseCohort(CourseCohort courseCohort, Integer teacherId) {
+        if (!courseCohort.getLectureTeacher().getId().equals(teacherId)) {
+            String cohortName = courseCohort.getCohort().getName();
+            String courseName = courseCohort.getCourse().getName();
+            throw new AccessDeniedException("You are not allowed to access lecture materials for course '" + courseName + "' and cohort '" + cohortName + "'");
+        }
+    }
+
+    void validateTeacherOwnsMaterial(Material material, Integer teacherId) {
+        if (!material.getTeacher().getId().equals(teacherId)) {
+            throw new AccessDeniedException("You are not allowed to access this material");
+        }
+    }
+}
