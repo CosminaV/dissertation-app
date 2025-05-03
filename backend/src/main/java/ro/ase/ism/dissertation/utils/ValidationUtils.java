6db46b027e8fcfc40305d6f882bce82803a25189
CosminaV <114497479+CosminaV@users.noreply.github.com>
2025-05-03 15:18:02 +0300

    feat(backend): added method to validate the academic year

--- backend/src/main/java/ro/ase/ism/dissertation/utils/ValidationUtils.java
+++ backend/src/main/java/ro/ase/ism/dissertation/utils/ValidationUtils.java
@@ -1,8 +1,12 @@
 package ro.ase.ism.dissertation.utils;
 
+import org.springframework.security.access.AccessDeniedException;
 import ro.ase.ism.dissertation.config.EducationLevelConfig;
 import ro.ase.ism.dissertation.exception.StudyYearNotValidException;
 import ro.ase.ism.dissertation.model.course.EducationLevel;
+
+import java.time.LocalDate;
+import java.time.Year;
 
 public class ValidationUtils {
     private ValidationUtils() {
@@ -14,5 +18,18 @@ public class ValidationUtils {
             throw new StudyYearNotValidException("Invalid year of study for " + educationLevel +
                     ": must be between 1 and " + max + ".");
         }
+    }
+
+    public static void validateAcademicYearUpdates(int year, String message) {
+        var currentAcademicYear = getCurrentAcademicYear();
+
+        if (currentAcademicYear != year) {
+            throw new AccessDeniedException(message);
+        }
+    }
+
+    public static int getCurrentAcademicYear() {
+        var now = LocalDate.now();
+        return (now.getMonthValue() >= 9) ? now.getYear() : now.getYear() -1;
     }
 }
