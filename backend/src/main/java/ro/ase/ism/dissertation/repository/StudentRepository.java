7f793e27dff16c9d9bfd9f8c39910ef092b13826
CosminaV <114497479+CosminaV@users.noreply.github.com>
2025-06-17 00:31:25 +0300

    fix(backend): updates on the StudentResponse & StudentRepository

--- backend/src/main/java/ro/ase/ism/dissertation/repository/StudentRepository.java
+++ backend/src/main/java/ro/ase/ism/dissertation/repository/StudentRepository.java
@@ -2,8 +2,16 @@ package ro.ase.ism.dissertation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ro.ase.ism.dissertation.model.user.Student;
 
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Integer>, JpaSpecificationExecutor<Student> {
+    @Query("""
+    SELECT s FROM Student s
+    WHERE s.studentGroup.cohort.id = :cohortId
+""")
+    List<Student> findByCohortId(@Param("cohortId") Integer cohortId);
 }
